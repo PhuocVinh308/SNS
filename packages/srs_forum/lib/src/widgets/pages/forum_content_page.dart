062407b27d8b512a48e952df41b652402a18bdf7
huynhthanhbach <54047061+huynhthanhbach@users.noreply.github.com>
2025-03-18 22:36:00 +0700

    commit

--- packages/srs_forum/lib/src/widgets/pages/forum_content_page.dart
+++ packages/srs_forum/lib/src/widgets/pages/forum_content_page.dart
@@ -15,72 +15,60 @@ class ForumContentPage extends GetView<ForumContentController> {
         child: Scaffold(
           resizeToAvoidBottomInset: true,
           backgroundColor: CustomColors.colorF2F2F2,
-          body: Container(
-            padding: EdgeInsets.only(bottom: 10.sp),
-            height: 1.sh,
-            width: 1.sw,
-            child: Stack(
-              children: [
-                Column(
+          body: Column(
+            children: [
+              const ForumContentAppBar(),
+              const Expanded(
+                child: ForumContentBody(),
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 15.sp),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(8.sp),
+                    topRight: Radius.circular(8.sp),
+                  ),
+                ),
+                child: Column(
                   children: [
-                    const ForumContentAppBar(),
-                    const Expanded(
-                      child: ForumContentBody(),
+                    CustomTextField(
+                      prefixIcon: IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.add_photo_alternate_rounded,
+                          size: 30.sp,
+                          color: CustomColors.color06b252,
+                        ),
+                      ),
+                      suffixIcon: IconButton(
+                        onPressed: () {},
+                        icon: Icon(
+                          Icons.send,
+                          size: 30.sp,
+                          color: CustomColors.color06b252,
+                        ),
+                      ),
                     ),
-                    Align(
-                      alignment: Alignment.bottomCenter,
-                      child: Container(
-                        padding: EdgeInsets.symmetric(horizontal: 15.sp),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.only(
-                            topLeft: Radius.circular(8.sp),
-                            topRight: Radius.circular(8.sp),
+                    Padding(
+                      padding: EdgeInsets.only(top: 5.sp),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Expanded(child: CustomText('file.png')),
+                          IconButton(
+                            onPressed: () {},
+                            icon: const Icon(
+                              Icons.cancel,
+                              color: CustomColors.color06b252,
+                            ),
                           ),
-                        ),
-                        child: Column(
-                          children: [
-                            CustomTextField(
-                              prefixIcon: IconButton(
-                                onPressed: () {},
-                                icon: Icon(
-                                  Icons.add_photo_alternate_rounded,
-                                  size: 30.sp,
-                                  color: CustomColors.color06b252,
-                                ),
-                              ),
-                              suffixIcon: IconButton(
-                                onPressed: () {},
-                                icon: Icon(
-                                  Icons.send,
-                                  size: 30.sp,
-                                  color: CustomColors.color06b252,
-                                ),
-                              ),
-                            ),
-                            Padding(
-                              padding: EdgeInsets.only(top: 5.sp),
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                children: [
-                                  Expanded(child: CustomText('file.png')),
-                                  IconButton(
-                                    onPressed: () {},
-                                    icon: const Icon(
-                                      Icons.cancel,
-                                      color: CustomColors.color06b252,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                          ],
-                        ),
+                        ],
                       ),
                     ),
                   ],
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
